c707fbe311932b1fa52bccaa759ed1923202241a
zfrizwan-sudo <zf.rizwan@gmail.com>
2026-01-14 11:38:47 -0800

    Add my hello.cpp file

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: YOUR NAME
+// Section: YOUR SECTION
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
